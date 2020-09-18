b077f2a16c138576e8b4c1f818e3b3ea277b267a
Oleg <oleglambin@gmail.com>
2020-09-18 17:35:32 +0300

    sync work

--- api/src/sync.js
+++ api/src/sync.js
@@ -5,48 +5,55 @@ const PROJECT_NAME = "test";
 const url = `http://localhost:8000/api/translations/download/${LANG}/${PROJECT_NAME}`;
 const urlUpload = "http://localhost:8000/api/translations/import-file";
 
-const request = require("request-promise");
+const requestPromise = require("request-promise");
+const request = require("request");
 
 const fs = require("fs");
 
 /**  GET REMOTE JSON  */
-request({
+requestPromise({
   url,
   method: "GET",
-  headers: {
-    Accept: "application/json",
-  },
+  json: true,
 })
   .then((remoteJson) => {
-    // const mergedJson = merge(JSON.parse(remoteJson), localJson);
-    console.log(" \n\n\n---\n\n\n ", remoteJson);
-    return;
-    // return new Promise(function (resolve, reject) {
-    //   fs.writeFile(
-    //     `${__dirname}/translations/ru.json`,
-    //     JSON.stringify(mergedJson, null, 2),
-    //     (err) => {
-    //       if (err) reject(err);
-    //       else resolve("saved");
-    //     }
-    //   );
-    // });
+    /**  MERGE LOCAL AND REMOTE  */
+    const mergedJson = merge(remoteJson, localJson);
+
+    /**  SAVE TO LOCAL  */
+
+    return new Promise(function (resolve, reject) {
+      fs.writeFile(
+        `${__dirname}/translations/ru.json`,
+        JSON.stringify(mergedJson, null, 2),
+        (err) => {
+          if (err) reject(err);
+          else resolve(true);
+        }
+      );
+    });
   })
-  .then((res) => {
-    if (res === "saved") {
-      console.log("sssssss");
-      // setTimeout(() => {
-      //   const req = request.put(urlUpload).then((res) => console.log(res));
-      //   const form = req.form();
-      //   form.append(
-      //     "filedata",
-      //     fs.createReadStream(`${__dirname}/translations/ru.json`)
-      //   );
-      //   form.append("filename", `${LANG}.json`);
-      //   form.append("account_id", "1");
-      //   form.append("pname", "test");
-      //   form.append("deleteOldKeys", "false");
-      //   form.append("doBackup", "true");
-      // }, 3000);
+  .then((saved) => {
+    if (saved) {
+      setTimeout(() => {
+        const req = request.put(urlUpload, function (err, resp, body) {
+          if (err) {
+            console.error("Error!", err.message);
+          } else {
+            console.log("Result: " + body);
+          }
+        });
+        const form = req.form();
+        form.append(
+          "filedata",
+          fs.createReadStream(`${__dirname}/translations/ru.json`)
+        );
+        form.append("filename", `${LANG}.json`);
+        form.append("account_id", "1");
+        form.append("pname", "test");
+        form.append("deleteOldKeys", "false");
+        form.append("doBackup", "true");
+      }, 2000);
     }
-  });
+  })
+  .catch((err) => console.error(err.message));
